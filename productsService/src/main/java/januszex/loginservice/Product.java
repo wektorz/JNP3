18aa82c5c142cd5d1f412fe4c43b3536b8d4b305
Wiktor Zdrojewski <wiktorzd@wp.pl>
2022-02-13 18:35:14 +0100

    pierwsza wersja sharded

--- productsService/src/main/java/januszex/loginservice/Product.java
+++ productsService/src/main/java/januszex/loginservice/Product.java
@@ -4,13 +4,19 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Sharded;
+
+import java.util.Random;
 
 
 @Data
 @Document
+@Sharded(shardKey = { "mod"} )
 public class Product {
     @Id
     private String id;
+
+    private Integer mod;
 
     private String desc;
     private float price;
@@ -18,6 +24,8 @@ public class Product {
 
     Product(String desc, float price, String img)
     {
+        Random random = new Random();
+        this.mod=  (random.nextInt() % 2 +2 ) % 2;
         this.desc=desc;
         this.price=price;
         this.img=img;
